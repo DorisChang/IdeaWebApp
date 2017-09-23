b551edc03aab74275664fb5d7baec3e4680f839e
Clarissa Xu <clarissa_xu24@outlook.com>
2017-09-23 13:07:07 +0000

    database start

--- building_sinatra_blog_app/app.rb
+++ building_sinatra_blog_app/app.rb
@@ -1,11 +1,14 @@
 # app.rb
+
+require 'bundler'
+Bundler.require
 require "sinatra"
 require "sinatra/activerecord"
 require "./models.rb"
 
 set :database, "sqlite3:myblogdb.sqlite3"
 class ApplicationController < Sinatra::Base
-    get “/“ do
+    get "/" do
     	@posts = Post.all
     	erb :index
     end
